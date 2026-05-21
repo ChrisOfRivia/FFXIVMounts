0df369b26dcd0c68fabf0b91b5e503a457cac4f0
Християн Върбанов <87647232+ChrisOfRivia@users.noreply.github.com>
2026-05-21 21:14:50 +0300

    API Mounts and slight UI
    Gathered mounts using API from ffxivcollect's free API and rearanged them to fit nicely with some info below

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
+import { useEffect, useState } from "react"
+
 function App() {
-  const mounts = [
-    {
-      id: 1,
-      name: "Alte Roite",
-      owned: false,
-    },
-    {
-      id: 2,
-      name: "Argos",
-      owned: true,
-    },
-    {
-      id: 3,
-      name: "Megashiba",
-      owned: false,
-    },
-  ]
-    
+  const [mounts, setMounts] = useState([])
+
+  useEffect(() => {
+    fetch("https://ffxivcollect.com/api/mounts")
+      .then((response) => response.json())
+      .then((data) => {
+        setMounts(data.results)
+      })
+  }, [])
+
 
   return (
     <div>
-      <h1> FFXIV Mount Tracker</h1>
+      <h1>FFXIV Mount Tracker</h1>
+      <div className="mount-grid">
+        {mounts.map((mount) => (
+          <div key={mount.id} className="mount-card">
+            <h2>{mount.name}</h2>
 
-      {mounts.map((mount) => (
-      <div key={mount.id}>
-        <h2> {mount.name} </h2>
-        <p> {mount.owned ? "Owned" : "Not Owned"}</p>
+            <img src={mount.image} />
+            <h4>Owned by: {mount.owned}</h4>
+
+          </div>
+
+        ))}
       </div>
-    ))}
     </div>
-    
   )
 }
 
